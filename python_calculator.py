6a799b3f0f097f7dd7b94159f902b573602f487b
Annie Bedford <abedford@gmail.com>
2020-10-16 17:06:56 +0100

    More changes to the calculator

--- python_calculator.py
+++ python_calculator.py
@@ -20,8 +20,7 @@ with open("step3.txt", "r") as f:
     list_of_calculations = f.readlines()
 
 index = 1
-processed_lines = {}
-processed_lines_counter = 0
+processed_indices  = []
 print("There are {0} lines in this file.".format(len(list_of_calculations)))
 
 not_revisiting = True
@@ -29,7 +28,7 @@ while index in range(1, len(list_of_calculations)) and not_revisiting:
     # get the line in the list (we have to take off 1 because line numbers in the list don't start at 0) 
     line= list_of_calculations[index-1]
     line = line.strip() 
-    if (line in processed_lines):
+    if (index in processed_indices ):
         not_revisiting = False
         print("We have seen line number {0} before {1} so exiting the program".format(index, line))
         break
@@ -40,19 +39,19 @@ while index in range(1, len(list_of_calculations)) and not_revisiting:
         param1int = int(values[3])
         param2int = int(values[4])
         print(line)
-        loc = calculate(param1int, values[2], param2int)
+        new_line_number = calculate(param1int, values[2], param2int)
             
     elif (values[0] == "goto"):
-        loc = int(values[1])
-        print("Goto {0}".format(loc))
+        new_line_number = int(values[1])
+        print("Goto {0}".format(new_line_number))
     elif (values[0] == "remove"):
-        loc = int(values[1])
-        line_to_remove = list_of_calculations[loc-1]       
-        print("Removing line {0} which is {1}".format(loc, line_to_remove))
-        if (0 <= loc-1 < len(list_of_calculations)-1):
+        new_line_number = int(values[1])
+        line_to_remove = list_of_calculations[new_line_number-1]       
+        print("Removing line {0} which is {1}".format(new_line_number, line_to_remove))
+        if (0 <= new_line_number-1 < len(list_of_calculations)-1):
             list_of_calculations.remove(line_to_remove)
-        loc = index + 1
-        print("Setting next location to {0}".format(loc))
+        new_line_number = index + 1
+        print("Setting next location to {0}".format(new_line_number))
 
     elif (values[0] == "replace"):
         line_number_to_be_replaced = values[1]
@@ -61,17 +60,16 @@ while index in range(1, len(list_of_calculations)) and not_revisiting:
         if ((0 <= line_number_to_be_replaced - 1 < len(list_of_calculations)-1) and (0 <= line_number_to_replace - 1 < len(list_of_calculations)-1)):
             line_to_replace = list_of_calculations[line_number_to_replace-1]
             list_of_calculations[line_number_to_be_replaced-1] = line_to_replace
-        loc = index+1
+        new_line_number = index+1
     else:
         print("Got unexpected value: {0}".format(values[1]))
 
-    processed_lines[processed_lines_counter] = line
-    processed_lines_counter += 1
-    print("Setting new index to {0}".format(loc))
-    print("Have now processed {0} lines.".format(processed_lines_counter))
-    index = loc 
+    processed_indices.append(index)
+    print("Setting new index to {0}".format(new_line_number))
+    print("Have now processed {0} lines.".format(len(processed_indices )))
+    index = new_line_number 
 
-print("Processed {0} lines".format(processed_lines_counter)) 
+print("Processed {0} lines".format(processed_indices)) 
 if (not_revisiting):
     print("Went outside the range")
 else:
